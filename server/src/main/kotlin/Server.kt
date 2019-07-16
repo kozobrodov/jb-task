b61594ee6c795bf2dafb6092137aeb5597f6e75a
Eugene Kozobrodov <eugene.kozobrodov@gmail.com>
2019-07-16 20:14:54 +0300

    Connect Ktor

--- server/src/main/kotlin/Server.kt
+++ server/src/main/kotlin/Server.kt
@@ -1,5 +1,20 @@
 package ru.kozobrodov
 
-fun main() {
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.*
 
+fun main() {
+    val server = embeddedServer(Netty, port = 8081) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Plain)
+            }
+        }
+    }
+    server.start(true)
 }
