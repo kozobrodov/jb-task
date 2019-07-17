50e67c0ede4a625bcf52ecd037ca96f55fd28a92
Eugene Kozobrodov <eugene.kozobrodov@gmail.com>
2019-07-17 21:31:00 +0300

    Enable console logging, handle exceptions

--- server/src/main/kotlin/Server.kt
+++ server/src/main/kotlin/Server.kt
@@ -5,16 +5,28 @@ import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
+import io.ktor.http.HttpStatusCode
 import io.ktor.jackson.jackson
 import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import java.io.FileNotFoundException
 
 @Suppress("unused")
 fun Application.module() {
     install(ContentNegotiation) {
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    install(StatusPages) {
+        exception<UnsupportedOperationException> {
+            call.respond(HttpStatusCode.NotImplemented, it.message ?: "Not implemented")
+        }
+        exception<FileNotFoundException> {
+            call.respond(HttpStatusCode.NotFound)
         }
     }
 
