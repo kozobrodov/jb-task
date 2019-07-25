5e379b1e0cdaf2b72c47b6fdc4da8d6b5dd24d9c
Eugene Kozobrodov <eugene.kozobrodov@gmail.com>
2019-07-25 19:53:08 +0300

    JQ plugin: add archive type to appropriate icons

--- frontend/js/fileTree.js
+++ frontend/js/fileTree.js
@@ -421,9 +421,10 @@
         typeToIconClassMap: {
             "directory": "fas fa-folder",
             "application/pdf": "fas fa-file-pdf",
-            "application/x-rar": "fas fa-file-archive",
-            "application/x-rar-compressed": "fas fa-file-archive",
-            "application/zip": "fas fa-file-archive",
+            "application/x-rar": "fas fa-file-archive rar",
+            "application/x-rar-compressed": "fas fa-file-archive rar",
+            "application/zip": "fas fa-file-archive zip",
+            "application/x-zip-compressed": "fas fa-file-archive zip",
             "application/x-java-archive": "fab fa-java",
             "application/java-archive": "fab fa-java",
             "<unknown_type>": "fas fa-file",
